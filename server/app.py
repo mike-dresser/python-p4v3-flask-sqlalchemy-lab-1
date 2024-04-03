8825ea30a840fc4b278cc356a4a390f6f9e5917c
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-04-02 21:44:48 -0400

    Completed and passing

--- server/app.py
+++ server/app.py
@@ -21,7 +21,27 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquakes_by_id(id):
+    q = Earthquake.query.filter_by(id=id).first()
+    if q:
+        body = q.to_dict()
+        response = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        response = 404
+    return make_response(body, response)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_by_magnitude(magnitude):
+    q = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+    result = []
+    for quake in q:
+        result.append(quake.to_dict())
+    response = {'count': len(result), 
+                'quakes': result
+                }
+    return make_response(response, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
